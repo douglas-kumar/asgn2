95efbc5f9ccfcd4777716a663b332af222efe07b
Douglas <douglaskumar11@gmail.com>
2017-05-05 16:54:33 +1000

    Initial LogHandlerPizza tests

--- src/asgn2Tests/LogHandlerPizzaTests.java
+++ src/asgn2Tests/LogHandlerPizzaTests.java
@@ -1,11 +1,26 @@
 package asgn2Tests;
 
+import java.util.ArrayList;
 
-/** A class that tests the methods relating to the creation of Pizza objects in the asgn2Restaurant.LogHander class.
-* 
-* @author Person B
-* 
-*/
+import org.junit.Test;
+
+import asgn2Exceptions.LogHandlerException;
+import asgn2Exceptions.PizzaException;
+import asgn2Pizzas.Pizza;
+import asgn2Restaurant.LogHandler;
+
+/**
+ * A class that tests the methods relating to the creation of Pizza objects in
+ * the asgn2Restaurant.LogHander class.
+ * 
+ * @author Person B - Douglas
+ * 
+ */
 public class LogHandlerPizzaTests {
-	// TO DO	
+    ArrayList<Pizza> pizzas;
+
+    @Test
+    public void readFromLog1() throws PizzaException, LogHandlerException {
+        pizzas = LogHandler.populatePizzaDataset("20170101.txt");
+    }
 }
