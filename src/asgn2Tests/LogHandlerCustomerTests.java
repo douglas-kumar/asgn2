7392488a58c9da11829fddc055f70030a4f83a96
15caloke <calum.oke@connect.qut.edu.au>
2017-05-15 17:00:40 +1000

    Added Tests
    Added tests for PizzaRestaurantCustomer and LogHanlerCustomer

--- src/asgn2Tests/LogHandlerCustomerTests.java
+++ src/asgn2Tests/LogHandlerCustomerTests.java
@@ -1,10 +1,123 @@
 package asgn2Tests;
 
+import asgn2Restaurant.LogHandler;
+
+import asgn2Exceptions.*;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import asgn2Customers.Customer;
+
 /**
  * A class that tests the methods relating to the creation of Customer objects in the asgn2Restaurant.LogHander class.
  *
  * @author Person A
  */
 public class LogHandlerCustomerTests {
-	// TO DO
+	private static final double DELTA = 1e-15;
+	private static final int FIRST_LINE = 0;
+	private LogHandler log, nonInstanciatedLog;
+	private ArrayList<Customer> logFile, nonInstanciatedLogFile;
+	private String expectedString;
+	private int expectedResult;
+	private double expectedCalc;
+	
+	@Before
+	public void setUp() throws CustomerException, LogHandlerException {
+		log = new LogHandler();
+		logFile = new ArrayList<Customer>();
+		logFile = LogHandler.populateCustomerDataset("20170101.txt");
+	}
+	
+	@Test
+	public void checkNotNullLogHandlerObject() {
+		assertNotNull(log);
+	}
+	
+	@Test
+	public void checkCustomerListIsNotNull() {
+		assertNotNull(logFile);
+	}
+	
+	@Test
+	public void checkNonInstanciatedLogHandlerObjectIsNull() {
+		assertNull(nonInstanciatedLog);
+	}
+	
+	@Test
+	public void checkNonInstanciatedCustomerListObjectIsNull() {
+		assertNull(nonInstanciatedLogFile);
+	}
+	
+	@Test
+	public void readsFileSuccessfully() throws CustomerException, LogHandlerException {
+		LogHandler.populateCustomerDataset("20170101.txt");
+	}
+	
+	@Test
+	public void readsCustomerNameCorrectlyFromFile() {
+		expectedString = "Casey Jones";
+		assertEquals(expectedString, logFile.get(FIRST_LINE).getName());
+	}
+	
+	@Test
+	public void readsCustomerNumCorrectlyFromFile() {
+		expectedString = "0123456789";
+		assertEquals(expectedString, logFile.get(FIRST_LINE).getMobileNumber());
+	}
+	
+	@Test
+	public void readsCustomerTypeCorrectlyFromFile() {
+		expectedString = "Driver Delivery";
+		assertEquals(expectedString, logFile.get(FIRST_LINE).getCustomerType());
+	}
+	
+	@Test
+	public void readsCustomerXLocationCorrectlyFromFile() {
+		expectedResult = 5;
+		assertEquals(expectedResult, logFile.get(FIRST_LINE).getLocationX());
+	}
+	
+	@Test
+	public void readsCustomerYLocationCorrectlyFromFile() {
+		expectedCalc = 10.0;
+		assertEquals(expectedCalc, logFile.get(FIRST_LINE).getDeliveryDistance(), DELTA);
+	}
+	
+	@Test
+	public void checkSizeOfFileIsCorrect() {
+		expectedResult = 3;
+		assertEquals(expectedResult, logFile.size());
+	}
+	
+	@Test
+	public void clearFunctionActuallyClearsAllLinesInLogFile() {
+		logFile.clear();
+		expectedResult = 0;
+		assertEquals(expectedResult, logFile.size());
+	}
+	
+	// -------------------- Exception Testing ------------------------------
+	
+	@Test(expected=LogHandlerException.class)
+	public void exceptionThrownWhenCreateCustomerWithEmptyData() throws CustomerException, LogHandlerException {
+		LogHandler.createCustomer("");
+	}
+	
+	@Test(expected=LogHandlerException.class)
+	public void exceptionThrownWhenPopulateEmptyData() throws CustomerException, LogHandlerException {
+		LogHandler.populateCustomerDataset("");
+	}
+	
+	@Test(expected=LogHandlerException.class)
+	public void exceptionThrownWhenFileIsNotInLogDirectory() throws CustomerException, LogHandlerException {
+		LogHandler.populateCustomerDataset("story.txt");
+	}
+	
+	
 }
--- src/asgn2Tests/RestaurantCustomerTests.java
+++ src/asgn2Tests/RestaurantCustomerTests.java
@@ -1,5 +1,18 @@
 package asgn2Tests;
 
+import asgn2Restaurant.LogHandler;
+import asgn2Restaurant.PizzaRestaurant;
+
+import asgn2Exceptions.*;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import asgn2Customers.Customer;
 
 /**
  * A class that that tests the methods relating to the handling of Customer objects in the asgn2Restaurant.PizzaRestaurant
@@ -8,5 +21,164 @@ package asgn2Tests;
  * @author Person A
  */
 public class RestaurantCustomerTests {
-	// TO DO
+	private static final double DELTA = 1e-15;
+	private static final int FIRST_LINE = 0, EMPTY_LOG_LINE = 4, 
+			NEGATIVE_LINE_NUM = -1;
+	private static final String FIRST_LOG_FILE = "20170101.txt";
+	private PizzaRestaurant pr, nonInstanciatedPR;
+	private int expectedResult;
+	private String expectedString;
+	private double expectedCalc;
+	
+	@Before
+	public void setUp() {
+		pr = new PizzaRestaurant();
+	}
+	
+	// Key: PR = Pizza Restaurant
+	
+	@Test
+	public void checkInstanciatedPRIsNotNull() {
+		assertNotNull(pr);
+	}
+	
+	@Test
+	public void checkNonInstanciatedPRIsNull() {
+		assertNull(nonInstanciatedPR);
+	}
+	
+	@Test
+	public void readsFileProperly() throws CustomerException, 
+	PizzaException, LogHandlerException {
+		assertTrue(pr.processLog(FIRST_LOG_FILE));
+	}
+	
+	@Test
+	public void testNumOfOrdersIsZeroWithoutProcessingLog() {
+		expectedResult = 0;
+		assertEquals(expectedResult, pr.getNumCustomerOrders());
+	}
+	
+	@Test
+	public void checkCorrectNumOfOrdersAfterProcessingLog() throws CustomerException, 
+	PizzaException, LogHandlerException {
+		pr.processLog(FIRST_LOG_FILE);
+		expectedResult = 3;
+		assertEquals(expectedResult, pr.getNumCustomerOrders());
+	}
+	
+	@Test
+	public void readsCustomerNameProperlyAfterProcessLog() throws CustomerException, 
+	PizzaException, LogHandlerException {
+		pr.processLog(FIRST_LOG_FILE);
+		expectedString = "Casey Jones";
+		assertEquals(expectedString, pr.getCustomerByIndex(FIRST_LINE).getName());
+	}
+	
+	@Test
+	public void readsCustomerNumProperlyAfterProcessLog() throws CustomerException, 
+	PizzaException, LogHandlerException {
+		pr.processLog(FIRST_LOG_FILE);
+		expectedString = "0123456789";
+		assertEquals(expectedString, pr.getCustomerByIndex(FIRST_LINE).getMobileNumber());
+	}
+	
+	@Test
+	public void readsCustomerTypeProperlyAfterProcessLog() throws CustomerException, 
+	PizzaException, LogHandlerException {
+		pr.processLog(FIRST_LOG_FILE);
+		expectedString = "Driver Delivery";
+		assertEquals(expectedString, pr.getCustomerByIndex(FIRST_LINE).getCustomerType());
+	}
+	
+	@Test
+	public void readsCustomerXLocationProperlyAfterProcessLog() throws CustomerException, 
+	PizzaException, LogHandlerException {
+		pr.processLog(FIRST_LOG_FILE);
+		expectedResult = 5;
+		assertEquals(expectedResult, pr.getCustomerByIndex(FIRST_LINE).getLocationX());
+	}
+	
+	@Test
+	public void readsCustomerYLocationProperlyAfterProcessLog() throws CustomerException, 
+	PizzaException, LogHandlerException {
+		pr.processLog(FIRST_LOG_FILE);
+		expectedResult = 5;
+		assertEquals(expectedResult, pr.getCustomerByIndex(FIRST_LINE).getLocationY());
+	}
+	
+	@Test
+	public void readsCustomerDistanceLocationProperlyAfterProcessLog() throws CustomerException, 
+	PizzaException, LogHandlerException {
+		pr.processLog(FIRST_LOG_FILE);
+		expectedCalc = 10.0;
+		assertEquals(expectedCalc, pr.getCustomerByIndex(FIRST_LINE).getDeliveryDistance(), 
+				DELTA);
+	}
+	
+	@Test
+	public void LogFileProperlyResetsNumOfOrders() throws CustomerException, 
+	PizzaException, LogHandlerException {
+		pr.processLog(FIRST_LOG_FILE);
+		pr.resetDetails();
+		expectedResult = 0;
+		assertEquals(expectedResult, pr.getNumCustomerOrders());
+	}
+	
+	@Test
+	public void checkCustomerAtSameIndexIsSameCustomer() throws CustomerException, 
+	PizzaException, LogHandlerException {
+		// TO DO:
+		pr.processLog(FIRST_LOG_FILE);
+		assertSame(pr.getCustomerByIndex(FIRST_LINE), 
+				pr.getCustomerByIndex(FIRST_LINE));
+	}
+	
+	
+	@Test
+	public void getsCorrectTotalDeliveryDist() throws CustomerException, 
+	PizzaException, LogHandlerException {
+		pr.processLog(FIRST_LOG_FILE);
+		// First Line TD = 10.0, Second Line TD = 5.0, Third Line = 0
+		expectedCalc = 15.0; // 10.0 + 5.0 + 0 = 15.0
+		assertEquals(expectedCalc, pr.getTotalDeliveryDistance(), DELTA);
+	}
+	
+	// ------------------ Exception Testing --------------------------
+	
+	@Test(expected=LogHandlerException.class)
+	public void exceptionThrownWhenInvalidFile() throws LogHandlerException, 
+	CustomerException, PizzaException {
+		pr.processLog("Invalid.txt");
+	}
+	
+	@Test(expected=LogHandlerException.class)
+	public void exceptionThrownWhenTextFileNotInLogFolder() throws LogHandlerException, 
+	CustomerException, PizzaException {
+		pr.processLog("story.txt"); // On my Desktop Directory
+	}
+	
+	@Test(expected=CustomerException.class)
+	public void exceptionThrownWhenCustomerIndexTooHigh() throws CustomerException, 
+	PizzaException, LogHandlerException {
+		pr.processLog(FIRST_LOG_FILE);
+		pr.getCustomerByIndex(EMPTY_LOG_LINE);
+	}
+	
+	@Test(expected=CustomerException.class)
+	public void exceptionThrownWhenCustomerIndexNegative() throws CustomerException, 
+	PizzaException, LogHandlerException {
+		pr.processLog(FIRST_LOG_FILE);
+		pr.getCustomerByIndex(NEGATIVE_LINE_NUM);
+	}
+	
+	@Test(expected=CustomerException.class)
+	public void exceptionThrownWhenGetCustomerAfterReset() throws CustomerException, 
+	PizzaException, LogHandlerException {
+		pr.processLog(FIRST_LOG_FILE);
+		pr.resetDetails();
+		expectedResult = 0;
+		pr.getCustomerByIndex(FIRST_LINE);
+	} 
+	
 }
